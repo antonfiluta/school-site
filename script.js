94e3d443fe0f17d969224a5e78bc652d17031c4d
antonfiluta <antonfiluta2.0@mail.ru>
2024-05-04 19:43:19 +0300

    feat: finish header

--- script.js
+++ script.js
@@ -13,12 +13,15 @@ function toScroll() {
        if (window.innerWidth < 700) {
 
        } else {
-          header.style.height = "7vw";
-          h1_wrapper.style.height = "7vw";
-          nav.style.height = "7vw";
+          header.style.height = "7.5vw";
+          h1_wrapper.style.height = "7.5vw";
+          nav.style.height = "7.5vw";
           nav_space_box.style.opacity = "0.0000001";
 
-          h1_bottom_text.style.display = "none";
+          nav_buttons_box.style.bottom = "0"
+
+          h1_top_text.style.fontSize = "7vw";
+          h1_bottom_text.style.opacity = "0.0000001";
        }
     } else {
         if (window.innerWidth < 700) {
@@ -29,7 +32,10 @@ function toScroll() {
           nav.style.height = "11.5vw";
           nav_space_box.style.opacity = "1";
 
-          h1_bottom_text.style.display = 'block';
+          nav_buttons_box.style.bottom = "-0.3vw"
+
+          h1_top_text.style.fontSize = "6.5vw";
+          h1_bottom_text.style.opacity = "1";
         }
     }
 }
